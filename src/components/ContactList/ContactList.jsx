bcc2ff825a27d72a3169f62ffd0a49d2fbf6ac42
uliacop <115625925+uliacop@users.noreply.github.com>
2024-06-18 20:14:43 +0300

    update hw acording tasks

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,13 +2,21 @@ import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 import { useSelector } from "react-redux";
 import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
 
 export default function ContactList() {
   const contacts = useSelector(selectContacts);
+  const nameFilter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter(
+    (contact) =>
+      contact.name.toLowerCase().includes(nameFilter.toLowerCase()) ||
+      contact.number.includes(nameFilter)
+  );
 
   return (
     <ul className={css.list}>
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <li className={css.item} key={contact.id}>
           <Contact data={contact} />
         </li>
--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -13,5 +13,4 @@ const filtersSlice = createSlice({
 
 export const { changeFilter } = filtersSlice.actions;
 export const filtersReducer = filtersSlice.reducer;
-
 export const selectNameFilter = (state) => state.filters.name;
